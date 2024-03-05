9803d1b24ca6710e4e73fdf50b788b9195c5b170
jahnhtl <philipp.jahn@htlwien10.at>
2024-03-05 12:55:32 +0100

    Some random minimum values on left/right sensors

--- source/arduino/autonom_car_plattform_io/src/sensors.cpp
+++ source/arduino/autonom_car_plattform_io/src/sensors.cpp
@@ -15,42 +15,40 @@ int16_t diff_left_right;
 
 void measure_distances()
 {
-  int16_t ir_sensor_front_new, ir_sensor_right_new, ir_sensor_left_new;
-  static int16_t ir_sensor_front_last, ir_sensor_right_last, ir_sensor_left_last;
-  static int16_t ir_sensor_front_2nd_last, ir_sensor_right_2nd_last, ir_sensor_left_2nd_last;
   int16_t ir_sensor_front_raw, ir_sensor_right_raw, ir_sensor_left_raw;
-
+  int16_t ir_sensor_front_new, ir_sensor_right_new, ir_sensor_left_new;
+  /*static int16_t ir_sensor_front_last, ir_sensor_right_last, ir_sensor_left_last;
+  static int16_t ir_sensor_front_2nd_last, ir_sensor_right_2nd_last, ir_sensor_left_2nd_last;*/
+   
   // read IR sensor data
   ir_sensor_front_raw = analogRead(A0);//sensorFront.getDistance();
 	ir_sensor_front_new = (int16_t) (16256 / (ir_sensor_front_raw + 22.8)) - 8;
-				if(ir_sensor_front_new > 150)
-          ir_sensor_front_new = 151;
-				else if(ir_sensor_front_new < 20)
-          ir_sensor_front_new = 19;
+
+	if(ir_sensor_front_new > 150)
+    ir_sensor_front_new = 151;
+	else if(ir_sensor_front_new < 20)
+    ir_sensor_front_new = 19;
 
   ir_sensor_right_raw = analogRead(A1);//sensorRight.getDistance();
-		ir_sensor_right_new = (int16_t) (6000 / (ir_sensor_right_raw - 6.89)) - 4;
-				if(ir_sensor_right_new > 80)
-          ir_sensor_right_new = 81;
-				else if(ir_sensor_right_new < 10)
-          ir_sensor_right_new = 9;
+	ir_sensor_right_new = (int16_t) (6000 / (ir_sensor_right_raw - 6.89)) - 4;
+
+	if(ir_sensor_right_new > 80)
+    ir_sensor_right_new = 81;
+	else if(ir_sensor_right_new < 10)
+    ir_sensor_right_new = 9;
 
   ir_sensor_left_raw = analogRead(A2);//sensorLeft.getDistance();
-		ir_sensor_left_new = (int16_t) (6000 / (ir_sensor_left_raw - 6.89)) - 4;
-				if(ir_sensor_left_new > 80)
-          ir_sensor_left_new = 81;
-				else if(ir_sensor_left_new < 10)
-          ir_sensor_left_new = 9;
+	ir_sensor_left_new = (int16_t) (6000 / (ir_sensor_left_raw - 6.89)) - 4;
 
-  // arithmetic mean to compensate wrong measurements
+	if(ir_sensor_left_new > 80)
+    ir_sensor_left_new = 81;
+	else if(ir_sensor_left_new < 10)
+    ir_sensor_left_new = 9;
+
   ir_sensor_front = ir_sensor_front_new;
   ir_sensor_right = ir_sensor_right_new;
   ir_sensor_left = ir_sensor_left_new;
 
-  /*ir_sensor_front = (ir_sensor_front + ir_sensor_front_new) / 2;
-  ir_sensor_right = (ir_sensor_right + ir_sensor_right_new) / 2;
-  ir_sensor_left = (ir_sensor_left + ir_sensor_left_new) / 2;*/
-  
   // calculate difference of left and right
   diff_left_right = abs(ir_sensor_right - ir_sensor_left);
 
