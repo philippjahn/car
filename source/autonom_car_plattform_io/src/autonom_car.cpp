b0fa10b6f1819eac7c6e2455f05ec7c99488c912
jahnhtl <philipp.jahn@htlwien10.at>
2025-02-17 10:12:40 +0100

    First somehow working version - 100 seconds

--- source/autonom_car_plattform_io/src/autonom_car.cpp
+++ source/autonom_car_plattform_io/src/autonom_car.cpp
@@ -179,12 +179,14 @@ void loop()
         
           if(ir_sensor_right < SIDE_DISTANCE)
           {
-            speed_left = diff16(speed_left, (SIDE_DISTANCE - ir_sensor_right)) * SIDECONTROL_FACTOR;
-            speed_right = add16(speed_right, (SIDE_DISTANCE - ir_sensor_right)) * SIDECONTROL_FACTOR;
+            speed_left = 0;
+            /*speed_left = diff16(speed_left, (SIDE_DISTANCE - ir_sensor_right)) * SIDECONTROL_FACTOR;
+            speed_right = add16(speed_right, (SIDE_DISTANCE - ir_sensor_right)) * SIDECONTROL_FACTOR;*/
           }
           else if(ir_sensor_right > SIDE_DISTANCE)
           {
-            speed_right = diff16(speed_right, (ir_sensor_right - SIDE_DISTANCE)) * SIDECONTROL_FACTOR;
+            speed_right = 0;
+            //speed_right = diff16(speed_right, (ir_sensor_right - SIDE_DISTANCE)) * SIDECONTROL_FACTOR;
           }
         #elif STRATEGY == 2 // SIDECONTROL LEFT
           if(ir_sensor_left > SIDE_DISTANCE)
